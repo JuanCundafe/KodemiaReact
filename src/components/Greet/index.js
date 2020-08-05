3a2a771f742350fdcc5169837d9b33015ab8bb95
Juan_Cundafé <juan.cundafe@crowdsense.app>
2020-08-04 20:07:41 -0500

    first commit

---
+++ src/components/Greet/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Greet(props) {
+  return (
+    <p>
+      Hola {props.firstName} {props.lastName}
+    </p>
+  );
+}
+
+export default Greet;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+/*
+function clock() {
+  const element = (
+    <div> 
+      <h1>{new Date().toLocaleTimeString()} </h1> 
+    </div>
+  )
+  ReactDOM.render(element, document.getElementById('root'))
+}
+
+setInterval(clock, 1000)*/
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
